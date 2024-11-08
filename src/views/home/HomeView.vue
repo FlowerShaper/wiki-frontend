57cb6addd2767b431fcf1b868ae7cd220ac2b149
flustix <me@flux.moe>
2024-11-08 17:56:10 +0100

    add very basic text to home view

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
+
 </script>
 
 <template>
-    meow
+    <div class="text-center flex flex-col py-5 gap-2">
+        <h1 class="text-4xl">Camellia Wiki</h1>
+        <h3 class="text-xl">Where most of Camellia's work the fandom is documented.</h3>
+    </div>
 </template>
