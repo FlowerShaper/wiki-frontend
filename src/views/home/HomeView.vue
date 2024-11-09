e819941dc5d04da62537f7628fb1375c5b7285d9
flustix <me@flux.moe>
2024-11-09 22:54:45 +0100

    add missing word

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -5,6 +5,6 @@
 <template>
     <div class="text-center flex flex-col py-5 gap-2">
         <h1 class="text-4xl">Camellia Wiki</h1>
-        <h3 class="text-xl">Where most of Camellia's work the fandom is documented.</h3>
+        <h3 class="text-xl">Where most of Camellia's work and the fandom is documented.</h3>
     </div>
 </template>
