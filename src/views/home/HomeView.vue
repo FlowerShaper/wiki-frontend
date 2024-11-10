61b6416d1c13a3c3f7f4e3d04a1e20a2592826ef
Aisuruneko <git@nekos.sh>
2024-11-10 16:47:28 -0500

    add (basic) design for homepage cards

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,10 +1,45 @@
 <script setup lang="ts">
 
+import CardView from './cards/CardView.vue';
+
+const bentos = [
+	{
+		section: "Wiki",
+		cards: [
+			{ title: "About", description: "All about the Camellia Wiki!", link: "/wiki/about" },
+			{ title: "Card title", description: "a very cool and long description that wraps across multiple lines", link: "/" },
+			{ title: "Card title", description: "a very cool and long description that wraps across multiple lines", link: "/" }
+		]
+	},
+	{
+		section: "Camellia",
+		cards: [
+			{ title: "Camellia", description: "The one and only Camellia! Here, you can find information about Camellia's background and accomplishments.", link: "/camellia" },
+			{ title: "Song Use", description: "Camellia's Music Usage Guidelines. Where you can find information about how and when to use his music!", link: "/camellia/song-use" },
+			{ title: "Card title", description: "a very cool and long description that wraps across multiple lines", link: "/" }
+		]
+	},
+	{
+		section: "Community",
+		cards: [
+			{ title: "Camellia Discord Server", description: "Information about the ever changing Camellia Discord Server.", link: "/community/server" },
+			{ title: "Card title", description: "a very cool and long description that wraps across multiple lines", link: "/" },
+			{ title: "Card title", description: "a very cool and long description that wraps across multiple lines", link: "/" }
+		]
+	}
+];
+
 </script>
 
 <template>
-    <div class="text-center flex flex-col py-5 gap-2">
-        <h1 class="text-4xl">Camellia Wiki</h1>
-        <h3 class="text-xl">Where most of Camellia's work and the fandom is documented.</h3>
-    </div>
+	<div class="text-center flex flex-col py-24 gap-2">
+		<h1 class="text-4xl">Camellia Wiki</h1>
+		<h3 class="text-xl">Where most of Camellia's work and the fandom is documented.</h3>
+	</div>
+	<div class="grid grid-cols-3 gap-4 w-full h-fit">
+		<div v-for="bento in bentos" :key="bento.section" class="grid gap-4">
+			<CardView v-for="card in bento.cards" :title="card.title" :description="card.description"
+				:link="card.link" />
+		</div>
+	</div>
 </template>
---
+++ src/views/home/cards/CardView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const props = defineProps<{
+	title?: string,
+	description?: string,
+	link: string
+}>();
+
+</script>
+
+<template>
+	<div class="grid p-6 bg-2 rounded-xl gap-2">
+		<div class="flex flex-col-reverse gap-1.5 mt-3 first:mt-0 scroll-m-20 w-full">
+			<div class="h-0.5 rounded w-16 from-secondary to-primary bg-gradient-to-r"></div>
+			<h3 class="text-xl">{{ props.title }}</h3>
+		</div>
+		<p class="w-full">{{ props.description }}</p>
+		<div class="flex justify-end y-w-full">
+			<a @click="router.push(props.link)"
+				class="w-fit text-right text-dark-accent cursor-pointer hover:text-secondary hover:underline">
+				read more
+			</a>
+		</div>
+	</div>
+</template>
