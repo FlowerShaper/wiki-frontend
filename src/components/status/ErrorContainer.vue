79040ac69e970f450ee9464b84e440ad6a90ed6c
flustix <me@flux.moe>
2024-11-16 23:26:29 +0100

    adjust loading and error container a little

--- src/components/status/ErrorContainer.vue
+++ src/components/status/ErrorContainer.vue
@@ -7,11 +7,11 @@ defineProps<{
 </script>
 
 <template>
-    <div class="w-full pt-8 flex flex-col items-center">
+    <div class="w-full pt-12 flex flex-col items-center">
         <div class="size-24 mb-4">
-            <LoadingImage class="size-full" src="https://cdn.discordapp.com/emojis/1214056479871471656.png?size=2048" />
+            <LoadingImage class="size-full rounded-lg" src="https://cdn.discordapp.com/emojis/1214056479871471656.png?size=2048" />
         </div>
-        <p>Something went wrong...</p>
+        <p class="font-semibold">something went wrong...</p>
         <p class="opacity-80 text-xs">{{ text }}</p>
     </div>
 </template>
