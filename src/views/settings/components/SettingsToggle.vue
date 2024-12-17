971f67e39412d124b8fd05da5fffc2ff9deeb105
flustix <me@flux.moe>
2024-12-17 16:36:50 +0100

    make the entire settings item clickable

--- src/views/settings/components/SettingsToggle.vue
+++ src/views/settings/components/SettingsToggle.vue
@@ -8,13 +8,12 @@ defineProps<{
 </script>
 
 <template>
-    <div class="w-full flex flex-row">
+    <div class="w-full flex flex-row" role="button" tabindex="0" @click="toggle" @keydown.enter="toggle">
         <div class="flex-1 flex flex-col">
             <p class="text-base text-primary">{{ title }}</p>
             <p class="text-sm">{{ description }}</p>
         </div>
-        <div class="size-11 flex flex-col items-center justify-center cursor-pointer" role="button" tabindex="0"
-            @click="toggle" @keydown.enter="toggle">
+        <div class="size-11 flex flex-col items-center justify-center cursor-pointer">
             <p class="text-2xl font-fa" v-if="active">{{ '\uf14a' }}</p>
             <p class="text-2xl font-fa-reg" v-else>{{ '\uf0c8' }}</p>
         </div>
