fd7e439a6128608d7e319d5b5694d12f8e01ef61
TheCodingGuy <git@nekos.sh>
2025-01-29 13:05:33 -0500

    fix article sidebar

--- src/views/article/ArticleView.vue
+++ src/views/article/ArticleView.vue
@@ -21,63 +21,63 @@ const split = route.path.split('/')
 const path = split.slice(1).join('/')
 
 const react = reactive<{
-    loading: boolean,
-    error?: string,
-    article?: WikiArticle
-    parsed?: ParsedMarkdown
-    content?: string
+	loading: boolean,
+	error?: string,
+	article?: WikiArticle
+	parsed?: ParsedMarkdown
+	content?: string
 }>({
-    loading: true
+	loading: true
 });
 
 watch(() => settings.language, FetchArticle)
 FetchArticle()
 
 function FetchArticle() {
-    react.loading = true
+	react.loading = true
 
-    API.PerformGet<WikiArticle>(`/articles?path=/${path}&lang=${settings.language}`).then(async res => {
-        if (!res.IsSuccess() || !res.data)
-            throw new Error(res.message)
+	API.PerformGet<WikiArticle>(`/articles?path=/${path}&lang=${settings.language}`).then(async res => {
+		if (!res.IsSuccess() || !res.data)
+			throw new Error(res.message)
 
-        react.article = res.data;
-        react.parsed = Markdown.Parse(res.data.content)
-        react.content = Markdown.Render(res.data.content)
-    }).catch(ex => {
-        react.error = ex.message
-    }).finally(() => react.loading = false)
+		react.article = res.data;
+		react.parsed = Markdown.Parse(res.data.content)
+		react.content = Markdown.Render(res.data.content)
+	}).catch(ex => {
+		react.error = ex.message
+	}).finally(() => react.loading = false)
 }
 </script>
 
 <template>
-    <PageBase>
-        <LoadingContainer v-if="react.loading" />
-        <ErrorContainer :text="react.error" v-else-if="react.error" />
-        <div class="flex flex-col gap-9" v-else>
-            <div class="flex flex-row w-full">
-                <div class="w-48 flex flex-col gap-2 sticky top-24 max-h-[36vh] overflow-y-auto">
-                    <h3 class="text-primary">Contents</h3>
-                    <ol class="list-decimal list-inside">
-                        <li class="text-lg" v-for="sec in react.parsed?.sections">
-                            <a class="hover:font-semibold" :href="`#${sec.id}`">{{ sec.title }}</a>
-                            <ul class="list-disc list-inside pl-4 text-base" v-if="sec.subs">
-                                <li v-for="sub in sec.subs">
-                                    <a class="hover:font-semibold" :href="`#${sub.id}`">{{ sub.title }}</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ol>
-                </div>
-                <div class="w-1 bg-2 rounded mx-6"></div>
-                <div class="flex flex-col flex-1 gap-4 max-w-full min-w-0">
-                    <div>
-                        <h1 class="text-4xl text-primary">{{ react.article?.meta.title }}</h1>
-                        <p class="opacity-75">written by {{ react.article?.meta.author }}</p>
-                    </div>
-                    <MarkdownView v-if="react.content" :content="react.content" />
-                </div>
-            </div>
-            <ArticleComments :path="path" />
-        </div>
-    </PageBase>
+	<PageBase>
+		<LoadingContainer v-if="react.loading" />
+		<ErrorContainer :text="react.error" v-else-if="react.error" />
+		<div class="flex flex-col gap-9" v-else>
+			<div class="flex flex-row w-full">
+				<div class="w-48 flex flex-col gap-2 sticky top-24 max-h-[36vh]">
+					<h3 class="text-primary">Contents</h3>
+					<ol class="list-decimal list-inside">
+						<li class="text-lg" v-for="sec in react.parsed?.sections">
+							<a class="hover:font-semibold" :href="`#${sec.id}`">{{ sec.title }}</a>
+							<ul class="list-disc list-inside pl-4 text-base" v-if="sec.subs">
+								<li v-for="sub in sec.subs">
+									<a class="hover:font-semibold" :href="`#${sub.id}`">{{ sub.title }}</a>
+								</li>
+							</ul>
+						</li>
+					</ol>
+				</div>
+				<div class="w-1 bg-2 rounded mx-6"></div>
+				<div class="flex flex-col flex-1 gap-4 max-w-full min-w-0">
+					<div>
+						<h1 class="text-4xl text-primary">{{ react.article?.meta.title }}</h1>
+						<p class="opacity-75">written by {{ react.article?.meta.author }}</p>
+					</div>
+					<MarkdownView v-if="react.content" :content="react.content" />
+				</div>
+			</div>
+			<ArticleComments :path="path" />
+		</div>
+	</PageBase>
 </template>
