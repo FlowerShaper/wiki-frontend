87dae2408fba2a02bb43ebb3bc11bb913367b641
flustix <me@flux.moe>
2025-01-29 18:41:43 +0100

    It's called serif not arial

--- src/views/settings/subviews/SettingsAppearance.vue
+++ src/views/settings/subviews/SettingsAppearance.vue
@@ -7,6 +7,6 @@ import { settings, UpdateSetting } from '@/utils/State';
 
 <template>
     <SettingsSubBase title="appearance">
-        <SettingsToggle :toggle="() => UpdateSetting(s => s.simpleFont = !s.simpleFont)" :active="settings.simpleFont" title="simple font" description="replaces the monospace font with a simpler, arial one." />
+        <SettingsToggle :toggle="() => UpdateSetting(s => s.simpleFont = !s.simpleFont)" :active="settings.simpleFont" title="simple font" description="replaces the monospace font with a simpler, serif one." />
     </SettingsSubBase>
 </template>
