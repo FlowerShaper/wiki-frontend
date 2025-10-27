2e86f07ad95b5a9fae4705ffcd5c966d8709de88
flustix <me@flux.moe>
2025-10-27 21:46:43 +0100

    open nav links in a new tab

--- src/components/navigation/bar.vue
+++ src/components/navigation/bar.vue
@@ -1,19 +1,18 @@
 <template>
-	<nav class="fixed left-0 top-0 z-20 h-16 w-full border-b-2 border-3 bg-1">
-		<div class="mx-auto flex h-full w-full flex-row items-center justify-between px-5 2xl:w-page">
-			<NuxtLink class="h-full py-2" to="/">
-				<LogoLong class="h-full" />
-			</NuxtLink>
-			<div class="flex flex-row items-center gap-4">
-				<a class="hidden hover:underline md:block" href="https://github.com/FlowerShaper">github</a>
-				<a class="hidden hover:underline md:block" href="https://discord.gg/camellia">discord</a>
-				<p class="hidden hover:underline md:block cursor-pointer"
-					@click="state.overlays.search = !state.overlays.search">search</p>
+    <nav class="fixed left-0 top-0 z-20 h-16 w-full border-b-2 border-3 bg-1">
+        <div class="mx-auto flex h-full w-full flex-row items-center justify-between px-5 2xl:w-page">
+            <NuxtLink class="h-full py-2" to="/">
+                <LogoLong class="h-full" />
+            </NuxtLink>
+            <div class="flex flex-row items-center gap-4">
+                <a class="hidden hover:underline md:block" target="_blank" href="https://github.com/FlowerShaper">github</a>
+                <a class="hidden hover:underline md:block" target="_blank" href="https://discord.gg/camellia">discord</a>
+                <p class="hidden cursor-pointer hover:underline md:block" @click="state.overlays.search = !state.overlays.search">search</p>
 
-				<div class="mx-2 hidden h-6 w-1 rounded bg-3 md:block"></div>
+                <div class="mx-2 hidden h-6 w-1 rounded bg-3 md:block"></div>
 
-				<NavigationProfile />
-			</div>
-		</div>
-	</nav>
+                <NavigationProfile />
+            </div>
+        </div>
+    </nav>
 </template>
