c7b7da60434fae5066e574c7f00779b0b9f1e62b
flustix <me@flux.moe>
2026-09-14 08:25:41 +0200

    fix favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,42 +6,20 @@ export default defineNuxtConfig({
     components: true,
     app: {
         head: {
-            htmlAttrs: {
-                'data-theme': 'dark',
-            },
+            htmlAttrs: { 'data-theme': 'dark' },
+            meta: [{ name: 'darkreader', content: 'meow' }],
             title: 'Camellia Wiki',
             viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
-            link: [
-                {
-                    rel: 'icon',
-                    type: 'image/x-icon',
-                    href: '/favicon.ico',
-                },
-            ],
-            meta: [{ name: 'darkreader', content: 'meow' }],
         },
-        pageTransition: {
-            name: 'fade',
-            mode: 'in-out',
-            type: 'transition',
-        },
-        layoutTransition: {
-            name: 'fade',
-        },
+        pageTransition: { name: 'fade', mode: 'in-out', type: 'transition' },
+        layoutTransition: { name: 'fade' },
     },
     css: ['~/assets/css/main.css', '~/assets/css/fonts.css'],
     extends: ['./wiki-lib'],
     icon: {
-        clientBundle: {
-            scan: true,
-        },
+        clientBundle: { scan: true },
         provider: 'server',
     },
-    experimental: {
-        asyncContext: true,
-    },
-    ui: {
-        theme: {  },
-        prose: false,
-    },
+    experimental: { asyncContext: true },
+    ui: { prose: false },
 });
