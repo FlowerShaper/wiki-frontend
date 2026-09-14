80b82da1a53d0ca78da7fa18bbda8266689eebd8
flustix <me@flux.moe>
2026-09-14 08:47:36 +0200

    replace link text with icons

--- app/components/navigation/bar.vue
+++ app/components/navigation/bar.vue
@@ -1,16 +1,34 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui';
+
+const icons: NavigationMenuItem[] = [
+    {
+        label: 'GitHub',
+        icon: 'ph:github-logo-bold',
+        to: 'https://github.com/FlowerShaper',
+    },
+    {
+        label: 'Discord',
+        icon: 'ph:discord-logo-bold',
+        to: 'https://discord.gg/camellia',
+    },
+    {
+        label: 'Search',
+        icon: 'ph:magnifying-glass-bold',
+        onSelect: () => (state.overlays.search = !state.overlays.search),
+    },
+];
+</script>
+
 <template>
     <nav class="fixed left-0 top-0 z-20 h-16 w-full border-b-2 border-background-3 bg-background-1">
         <div class="mx-auto flex h-full w-full flex-row items-center justify-between px-5 2xl:w-page">
             <NuxtLink class="h-full py-2" to="/">
                 <LogoLong class="h-full" />
             </NuxtLink>
-            <div class="flex flex-row items-center gap-4">
-                <a class="hidden hover:underline md:block" target="_blank" href="https://github.com/FlowerShaper">github</a>
-                <a class="hidden hover:underline md:block" target="_blank" href="https://discord.gg/camellia">discord</a>
-                <p class="hidden cursor-pointer hover:underline md:block" @click="state.overlays.search = !state.overlays.search">search</p>
-
-                <div class="mx-2 hidden h-6 w-1 rounded bg-background-3 md:block"></div>
-
+            <div class="flex flex-row items-center gap-2">
+                <UButton variant="ghost" color="neutral" :icon="i.icon" v-for="i in icons" :to="i.to" target="_blank" @click="i.onSelect" />
+                <div class="md:mx-4 h-6 w-1 rounded md:bg-background-3"></div>
                 <NavigationProfile />
             </div>
         </div>
